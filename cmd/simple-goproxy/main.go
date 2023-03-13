7f92b1930aec6d30550809b6079d6bb727884432
Mostafa Negim <mostafa.negim@gmail.com>
2023-03-13 22:30:13 +0330

    add log for debug

--- cmd/simple-goproxy/main.go
+++ cmd/simple-goproxy/main.go
@@ -2,18 +2,35 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	rawLog "log"
+	"os"
+
+	log "github.com/sirupsen/logrus"
 )
 
 type Config struct {
 	version bool
 
-	Addr string
+	Debug int
+	Addr  string
 }
 
 func main() {
 	config := loadConfig()
-	fmt.Println(config.Addr)
+
+	if config.Debug != 0 {
+		rawLog.SetFlags(rawLog.LstdFlags | rawLog.Lshortfile)
+		log.SetLevel(log.DebugLevel)
+	} else {
+		log.SetLevel(log.InfoLevel)
+	}
+	if config.Debug == 2 {
+		log.SetReportCaller(true)
+	}
+	log.SetOutput(os.Stdout)
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp: true,
+	})
 }
 
 func loadConfig() *Config {
@@ -25,6 +42,7 @@ func loadConfigFromCli() *Config {
 	config := new(Config)
 
 	flag.BoolVar(&config.version, "version", false, "show simple-goproxy version")
+	flag.IntVar(&config.Debug, "debug", 0, "debug mode: 1 - print debug log, 2 - show debug from")
 	flag.StringVar(&config.Addr, "addr", ":9080", "proxy listen addr")
 	flag.Parse()
 
