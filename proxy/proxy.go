1dd6c9351e9adf4144a9dde6b3e3169ecb941b39
Mostafa Negim <mostafa.negim@gmail.com>
2023-03-15 22:40:30 +0330

    add proxy.gl

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,7 +1,26 @@
 package proxy
 
+import "net/http"
+
 type Options struct {
-	Debug int
-	Addr string
+	Debug    int
+	Addr     string
 	Upstream string
+}
+
+type Proxy struct {
+	Opts    *Options
+	Version string
+
+	client *http.Client
+	server *http.Server
+}
+const VERSION = "1.6.0"
+func NewProxy(opts *Options) (*Proxy, error) {
+	proxy := &Proxy{
+		Opts: opts,
+		Version: VERSION,
+	}
+
+	return proxy, nil
 }
